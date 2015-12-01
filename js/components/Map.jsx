a54084cc5371ff5addfa2bef26d097abca5868b6
unknown <evboyle@mrroboto.redmond.corp.microsoft.com>
2015-12-01 15:15:17 -0800

    map view with pushpins and basic infobox

--- js/components/Map.jsx
+++ js/components/Map.jsx
@@ -5,8 +5,40 @@ var config = require('../config');
 var Map = React.createClass({
 	componentDidMount: function() {
 
-			var map = new Microsoft.Maps.Map(ReactDOM.findDOMNode(this), {credentials: config.bingAPIKey, width: 400, height: 400})
+			var map = new Microsoft.Maps.Map(ReactDOM.findDOMNode(this), {credentials: config.bingAPIKey, width: 400, height: 400});
+			this.setState({map: map});
+			this.setMapView(map, this.props.results)
 
+	},
+	
+	componentWillReceiveProps: function(nextProps) {
+		this.setMapView(this.state.map, nextProps.results);
+	},
+	
+	setMapView: function(map, results) {
+		
+		map.entities.clear();
+		var locs = [];
+		
+		var infoBox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(0, 0), { width: 150, height: 75, visible: false});
+		map.entities.push(infoBox);
+		
+		results.forEach(function(result) {
+			if(result.location) {
+				var location = new Microsoft.Maps.Location(result.location.coordinates[1], result.location.coordinates[0]);
+				var pushpin = new Microsoft.Maps.Pushpin(location, null);
+				pushpin.Title = result.title;
+				var createInfoBox = function(event){
+					infoBox.setLocation(location);
+					infoBox.setOptions({title: result.title, visible: true});
+				};
+				Microsoft.Maps.Events.addHandler(pushpin, 'click', createInfoBox)
+				map.entities.push(pushpin);
+				locs.push(location);
+			}
+		});
+		var viewBox = Microsoft.Maps.LocationRect.fromLocations(locs);
+		map.setView({bounds: viewBox});
 	},
 	
 	render: function() {
